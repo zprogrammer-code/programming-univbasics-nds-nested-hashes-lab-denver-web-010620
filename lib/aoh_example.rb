9b941d4f08d0948b6a11b345bc109fbef71559d8
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-28 16:25:00 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,8 +8,8 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
   people = [
-    {:name "Don Gately", :occupation "Live-in Staff"},
-    {:name "Joelle van Dyne", :occupation "Radio Personality"},
+    {:name => "Don Gately", :occupation => "Live-in Staff"},
+    {:name "Joelle van Dyne", :occupation +> "Radio Personality"},
     {:name "Pat Monteseian", :occupation "Staff"},
     {:name "Kate Gompert", :occupation "none"},
     {:name "Bruce Green", :occupation "Fan of Mildred"}]
