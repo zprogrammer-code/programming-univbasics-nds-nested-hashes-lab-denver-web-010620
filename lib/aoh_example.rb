955e008c4aeda0ed49b79f0a7f29ccd286d52a3a
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-28 16:34:55 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,11 +8,7 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
   result = [
-    {:name=>"Don Gately", :occupation=> "Live-in Staff"},
-    {:name=> "Joelle van Dyne", :occupation=> "Radio Personality"},
-    {:name=> "Pat Monteseian", :occupation=> "Staff"},
-    {:name=> "Kate Gompert", :occupation=> "None"},
-    {:name=> "Bruce Green", :occupation=> "Fan of Mildred"}]
+    {:name=>"Don Gately", :occupation=> "Live-in Staff"},{:name=> "Joelle van Dyne", :occupation=> "Radio Personality"},{:name=> "Pat Monteseian", :occupation=> "Staff"},{:name=> "Kate Gompert", :occupation=> "None"}, {:name=> "Bruce Green", :occupation=> "Fan of Mildred"}]
     
     return result
   # Build an array that contains (or "nests") the constants into a single
@@ -20,7 +16,14 @@ def assembled_aoh
 end
 
 def literal_aoh
- 
+ result = [
+    {:name=>"Don Gately", :occupation=> "Live-in Staff"},
+    {:name=> "Joelle van Dyne", :occupation=> "Radio Personality"},
+    {:name=> "Pat Monteseian", :occupation=> "Staff"},
+    {:name=> "Kate Gompert", :occupation=> "None"},
+    {:name=> "Bruce Green", :occupation=> "Fan of Mildred"}]
+    
+    return result
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
