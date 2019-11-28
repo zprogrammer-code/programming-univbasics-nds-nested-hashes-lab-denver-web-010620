f2acaa41324bcffa83f30011d1833734dc0f97db
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-28 16:30:52 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,7 +7,7 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  people = [
+  result = [
     {:name=>"Don Gately", :occupation=> "Live-in Staff"},
     {:name=> "Joelle van Dyne", :occupation=> "Radio Personality"},
     {:name=> "Pat Monteseian", :occupation=> "Staff"},
