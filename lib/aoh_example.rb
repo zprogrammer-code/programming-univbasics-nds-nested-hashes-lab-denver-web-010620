2632dafa3c70b4fbb7df4ac0fdcfdfa3e711079e
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-28 16:25:42 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -9,10 +9,10 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 def assembled_aoh
   people = [
     {:name => "Don Gately", :occupation => "Live-in Staff"},
-    {:name "Joelle van Dyne", :occupation +> "Radio Personality"},
-    {:name "Pat Monteseian", :occupation "Staff"},
-    {:name "Kate Gompert", :occupation "none"},
-    {:name "Bruce Green", :occupation "Fan of Mildred"}]
+    {:name => "Joelle van Dyne", :occupation => "Radio Personality"},
+    {:name => "Pat Monteseian", :occupation => "Staff"},
+    {:name => "Kate Gompert", :occupation => "none"},
+    {:name => "Bruce Green", :occupation => "Fan of Mildred"}]
   # Build an array that contains (or "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
 end
